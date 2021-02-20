aa9c86fcf70094deff7707c285d2e6de61fb81ee
Becca White <beccawhite@Beccas-MBP.home>
2021-02-20 15:30:17 -0800

    Basic HTML

--- main.js
+++ main.js
@@ -5,24 +5,23 @@ const cardDeck = {
 }
 
 
-
 /*----- app's state (variables) -----*/ //use JS to reference 
-let player1deck = [];
-let player2deck = [];
+let playerdeck = [];
+let computerdeck = [];
 let winner;
 
 
 
 /*----- cached element references -----*/ //what you SEE changing
 const deckCount = {
-    player1: document.querySelector(), // grab arr length of player1deck
-    player2: document.querySelector() // grab arr length of player2deck
+    player1: document.querySelector(), // grab arr length of playerdeck
+    player2: document.querySelector() // grab arr length of computerdeck
 }
 
 const moves = {
-    // grab and place the 0 idx of player1deck
+    // grab and place the 0 idx of playerdeck
     player1: document.querySelector(), 
-    // grab and place the 0 idx of player2deck
+    // grab and place the 0 idx of computerdeck
     player2: document.querySelector() 
 }
 
@@ -34,11 +33,17 @@ document.querySelector() // deal cards
 
 document.querySelector() //  place cards or start "round"
 
-
+document.querySelector(".deal")
+    .addEventListener("click", dealCards); // will need to call this function somewhere
 
 /*----- functions -----*/
+
 
 // initial value at start of game
 function init(){
 
+}
+
+function dealcards(){
+    console.log("dealing the cards...")
 }
