03bf991a4f5eb4a81b6a03be1e71eca911677a33
Becca White <beccawhite@Beccas-MBP.home>
2021-02-19 17:56:23 -0800

    Initial Commit

---
+++ main.js
@@ -0,0 +1,44 @@
+/*----- constants -----*/
+
+const cardDeck = {
+
+}
+
+
+
+/*----- app's state (variables) -----*/ //use JS to reference 
+let player1deck = [];
+let player2deck = [];
+let winner;
+
+
+
+/*----- cached element references -----*/ //what you SEE changing
+const deckCount = {
+    player1: document.querySelector(), // grab arr length of player1deck
+    player2: document.querySelector() // grab arr length of player2deck
+}
+
+const moves = {
+    // grab and place the 0 idx of player1deck
+    player1: document.querySelector(), 
+    // grab and place the 0 idx of player2deck
+    player2: document.querySelector() 
+}
+
+
+
+
+/*----- event listeners -----*/
+document.querySelector() // deal cards
+
+document.querySelector() //  place cards or start "round"
+
+
+
+/*----- functions -----*/
+
+// initial value at start of game
+function init(){
+
+}
