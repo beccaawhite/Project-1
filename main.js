7bfb86f5df2e9efa6b7c94b27087a66d22039f59
Becca White <beccawhite@Beccas-MBP.home>
2021-02-21 22:47:56 -0800

    Updated Deck Counts

--- main.js
+++ main.js
@@ -10,15 +10,13 @@ const name = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 let playerdeck = [];
 let computerdeck = [];
 let winner;
+let counts;
 
 let deck;
 
 
 /*----- cached element references -----*/ //what you SEE changing
-const deckCount = {
-    // player1: document.querySelector(), // grab arr length of playerdeck
-    // player2: document.querySelector() // grab arr length of computerdeck
-}
+
 
 const moves = {
     // grab and place the 0 idx of playerdeck
@@ -30,10 +28,15 @@ const moves = {
 /*----- event listeners -----*/
 
 
-// document.querySelector() //  place cards or start "round"
+// document.querySelector(".next-turn") //  place cards or start "round"
 
-// document.querySelector(".deal")
-//     .addEventListener("click", dealCards()); // will need to call this function somewhere
+document.querySelector(".deal")
+    .addEventListener("click", dealCards); 
+
+// document.querySelector(".reset")
+//     .addEventListener("click", init)
+
+
 
 /*----- functions -----*/
 
@@ -42,13 +45,24 @@ const moves = {
 init();
 
 function init(){
+    // initialize scores // doesn't change here 
+    counts = {
+        player: 0,
+        computer: 0
+    }
 
     getCards();
 
+    render();
+}
+
+function render() {
+    console.log("render is firing");
+
+    
 }
 
 function getCards(){ 
-
     deck = [];
     for (let i = 0; i < suits.length; i++){
         for(let j = 0; j < values.length; j++){
@@ -62,22 +76,20 @@ function getCards(){
     // return deck
 }
 
-// need to turn deck into array
 
 function shuffleCards(deck){
+    // generate random index and assign that to deck?
     for (let i = 0; i < cards.length; i++){
         Math.floor(Math.random() * cards.length)
     }
 }
 
 // find a  way to take the deck generated in getCards() and connect to visual cards
-function makeCard(suit, value){
+// function makeCard(suit, value){
     // grab the div in the HTML
-    document.querySelector(".deck").innerHTML = "";
+    // document.querySelector(".deck").innerHTML = "";
     // match the suit and value with the img from resources  
-
-
-}
+// }
 
 
 function dealCards(){
@@ -88,17 +100,34 @@ function dealCards(){
         computerdeck.push(deck[i]);
         i += 2;
     }
-    console.log(playerdeck)
-    console.log(computerdeck)
-
-    
-    // deck.forEach(function(i) {
-    //     if (deck[i] % 2 === 0) {
-    //         playerdeck.push(deck[i])
-    //     } else {
-    //         playerdeck.push(deck[i])
-    //     }
-    // })
-   
+    updateCardCount();
 }
+
+function updateCardCount(){
+    document.getElementById("player-score").innerHTML = "Count: " + playerdeck.length,
+    document.getElementById("computer-score").innerHTML = "Count: " + computerdeck.length
+}
+
+
+
+
+// once cards are connected to html and cards are placed on the right webpage spot
+
+// create function for next turn
+    // should take card on top and place it in middle face up
+    // compare cards value
+        // if player value greater than copmuter, push cards to end of player array
+        // if computer value greater than player, push cards to end of computer array
+        // if values are the same, place three more cards face up
+            // repeat process until winner
+
+
+// create function for winner
+    // if player or computer deck count === 52, game over
+    // create alert (or something) that says which player won
+    // style the winning player 
+
+
+// fix reset game to work
+
 
