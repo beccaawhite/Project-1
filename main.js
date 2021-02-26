01d32c120b0ea2bdaa08ec512ba422c1502834d7
Becca White <beccawhite@Beccas-MBP.home>
2021-02-25 18:42:47 -0800

    Completed MVP

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 
 const suits = ["h", "s", "d", "c"]; // naming it this way so that it matches the css file
 const ranks = ["02", "03", "04", "05", "06", "07", "08", "09", "10", "J", "Q", "K", "A"];
-const vals = ["02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14"];
 
 const masterDeck = getCards();
 
@@ -12,10 +11,8 @@ const masterDeck = getCards();
 
 let playerdeck = [];
 let computerdeck = [];
-let winner;
 let deck;
 let shuffledDeck;
-
 
 
 /*----- cached element references -----*/ //what you SEE changing
@@ -29,22 +26,12 @@ const computerWinner = document.querySelector(".computer ");
 const playerWinner = document.querySelector(".player");
 
 
-
-
-
-
 /*----- event listeners -----*/
 document.querySelector(".deal")
     .addEventListener("click", dealCards);
 
 document.querySelector(".next-turn")
-    .addEventListener("click", nextTurn); 
-
-
-//cant get this to reset
-document.querySelector(".reset")
-    .addEventListener("click", init)
-
+    .addEventListener("click", nextTurn);
 
 
 /*----- functions -----*/
@@ -54,69 +41,63 @@ document.querySelector(".reset")
 init();
 
 function init() {
-    // initialize scores // doesn't change here 
-    
-    
 
     render();
 }
 
 function render() {
-    console.log("render is firing");
-
+    // console.log("render is firing");
     getCards();
     shuffle();
-
-
 }
 
 // function to create a card deck with 52 cards
 function getCards() {
     const carddeck = [];
-    suits.forEach(function(suit){
-        ranks.forEach(function(rank){
+    suits.forEach(function (suit) {
+        ranks.forEach(function (rank) {
             let value = Number(rank)
             // if this is J --> value: 11
-            if (rank == "J"){
+            if (rank == "J") {
                 value = 11
-            } else if (rank == "Q"){
+            } else if (rank == "Q") {
                 value = 12
-            } else if (rank == "K"){
+            } else if (rank == "K") {
                 value = 13
             } else if (rank == "A") {
                 value = 14
             }
             carddeck.push({
                 //the "face" property maps to the lib CSS classes for cards .card.dA,
-                face: `${suit}${rank}`, 
+                face: `${suit}${rank}`,
                 // Setting the value property for a game of war
                 value: value,
-    
+
             })
         })
-    }) 
+    })
     return carddeck
 }
 
 
 // function to update DOM with card faces, called in dealCards()
-function renderDeck(deck, container){
+function renderDeck(deck, container) {
     container.innerHTML = [];
-    const cardsHtml = deck.reduce(function(html, card){
+    const cardsHtml = deck.reduce(function (html, card) {
         return html + `<div class="card large ${card.face}"></div>`
     }, []);
     container.innerHTML = cardsHtml;
-    
+
 }
 
 function shuffle() {
     const tempDeck = [...masterDeck];
     shuffledDeck = [];
     while (tempDeck.length) {
-      // Get a random index for a card still in the tempDeck
-      const rndIdx = Math.floor(Math.random() * tempDeck.length);
-      // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
-      shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+        // Get a random index for a card still in the tempDeck
+        const rndIdx = Math.floor(Math.random() * tempDeck.length);
+        // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+        shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
     }
     return shuffledDeck
     console.log(shuffledDeck)
@@ -125,8 +106,8 @@ function shuffle() {
 // function to split master deck into two players, render in DOM
 function dealCards() {
     console.log("dealing the cards...");
-    for(let i = 0; i < shuffledDeck.length; i++){
-        if(i%2){
+    for (let i = 0; i < shuffledDeck.length; i++) {
+        if (i % 2) {
             playerdeck.push(shuffledDeck[i]);
         } else {
             computerdeck.push(shuffledDeck[i]);
@@ -139,12 +120,10 @@ function dealCards() {
     computerContainer.classList.add("back-red");
     computerContainer.classList.add("card");
     computerContainer.classList.add("large");
-    
-
 }
 
 // function to get rid of back of card 
-function removeClass(){
+function removeClass() {
     playerContainer.classList.remove("back-red");
     playerContainer.classList.remove("card");
     computerContainer.classList.remove("back-red");
@@ -154,39 +133,39 @@ function removeClass(){
 // function to update the DOM with the length of each players deck array
 function updateCardCount() {
     document.getElementById("player-score").innerHTML = "Count: " + playerdeck.length,
-    document.getElementById("computer-score").innerHTML = "Count: " + computerdeck.length
+        document.getElementById("computer-score").innerHTML = "Count: " + computerdeck.length
 }
 
 
 // function to call ever turn, if either deck is 52, winner winner chicken dinner
-function chickenDinner(){
-    if((playerdeck.length === 52) || (computerdeck.length === 52)){
-        if(playerdeck.length === 52){
-            playerWinner.style.color = "#ff0054"; 
+function chickenDinner() {
+    if ((playerdeck.length === 52) || (computerdeck.length === 52)) {
+        if (playerdeck.length === 52) {
+            playerWinner.style.color = "#ff0054";
             playerWinner.style.fontSize = "25px"
             document.getElementById("player-score").innerHTML = "YOU WON! ";
             document.getElementById("computer-score").innerHTML = "Computer Lost";
+            playerContainer.classList.add("back-red");
+            playerContainer.classList.add("card");
+            playerContainer.classList.add("large");
+            computerContainer.innerHTML = ""
 
         }
         else {
-            computerWinner.style.color = "#ff0054"; 
+            computerWinner.style.color = "#ff0054";
             computerWinner.style.fontSize = "25px"
             document.getElementById("player-score").innerHTML = "You Lost";
             document.getElementById("computer-score").innerHTML = "WINNER";
             computerContainer.classList.add("back-red");
             computerContainer.classList.add("card");
+            computerContainer.classList.add("large");
+            playerContainer.innerHTML = ""
         }
     }
 }
 
-function show(){
-    warPlaceholder.innerHTML = "WAR!";
-}
-function showWar(){
-    placeholder.innerHTML = "someone won the war"
-}
 
-function nextTurn(){
+function nextTurn() {
     // takes off the back of card decor
     removeClass();
     placeholder.innerHTML = ""
@@ -197,17 +176,17 @@ function nextTurn(){
     // console.log(computerdeck[0].value)
 
     // if card values are the same, WAR
-    if (playerdeck[0].value === computerdeck[0].value){
-        
-        if((playerdeck.length > 3) && (computerdeck.length > 3)){
+    if (playerdeck[0].value === computerdeck[0].value) {
+
+        if ((playerdeck.length > 3) && (computerdeck.length > 3)) {
             // call small war function if either deck is 
             bigWar(playerdeck, computerdeck)
         }
         else {
             smallWar(playerdeck, computerdeck)
         }
-    // card that has greater value, wins    
-    } 
+        // card that has greater value, wins    
+    }
     else {
         compareValues(playerdeck, computerdeck);
     }
@@ -217,17 +196,17 @@ function nextTurn(){
 
 
 // function for if both players have an additional 3 cards to play
-function bigWar(deck1, deck2){
+function bigWar(deck1, deck2) {
     let warplay = [];
     let warcomp = [];
     // iterate and get 3 additional cards from each player
-    for (let i = 0; i < 4; i++){
-      let p1 = playerdeck.shift();
-      warplay.push(p1);
+    for (let i = 0; i < 4; i++) {
+        let p1 = playerdeck.shift();
+        warplay.push(p1);
     }
-    for (let i = 0; i < 4; i++){
-      let c1 = computerdeck.shift();
-      warcomp.push(c1)
+    for (let i = 0; i < 4; i++) {
+        let c1 = computerdeck.shift();
+        warcomp.push(c1)
     }
     // to see the war decks and whats being compared 
     console.log(`warplay:`)
@@ -236,13 +215,13 @@ function bigWar(deck1, deck2){
     console.log(warcomp);
     // compare the last placed card (last index)
     // whichever is larger in value, add array to end of deck
-    if(warplay[3].value > warcomp[3].value){
+    if (warplay[3].value > warcomp[3].value) {
         addArray(warplay, playerdeck);
         addArray(warcomp, playerdeck);
         console.log("player deck won")
         console.log(playerdeck);
         placeholder.innerHTML = "You won the war!"
-    } 
+    }
     else {
         addArray(warcomp, computerdeck);
         addArray(warplay, computerdeck);
@@ -250,7 +229,7 @@ function bigWar(deck1, deck2){
         console.log(computerdeck);
         placeholder.innerHTML = "Computer won the war"
         // placeholder.innerHTML = "Computer won the war"
-    
+
     }
     renderDeck(playerdeck, playerContainer);
     renderDeck(computerdeck, computerContainer);
@@ -258,19 +237,19 @@ function bigWar(deck1, deck2){
 }
 
 // function for if one deck doesn't have enough cards for war, they lose
-function smallWar(deck1, deck2){
+function smallWar(deck1, deck2) {
     // if player deck is smaller, copmuter gets all the cards
-    if(playerdeck.length < computerdeck.length){
+    if (playerdeck.length < computerdeck.length) {
         addArray(playerdeck, computerdeck)
         let warcomp = computerdeck.shift();
         addArray(warcomp, computerdeck)
         playerdeck = [];
-    } 
-    else{
-      addArray(computerdeck, playerdeck)
-      let warplay = playerdeck.shift();
-      addArray(warplay, playerdeck)
-      computerdeck = [];
+    }
+    else {
+        addArray(computerdeck, playerdeck)
+        let warplay = playerdeck.shift();
+        addArray(warplay, playerdeck)
+        computerdeck = [];
     }
     console.log(computerdeck)
     console.log(playerdeck)
@@ -279,9 +258,9 @@ function smallWar(deck1, deck2){
 
 // function to add won array to end of deck
 // otherwise deck gets added to array in an object
-function addArray(start, end){
+function addArray(start, end) {
     let arrIdx = 0;
-    while(arrIdx < start.length){
+    while (arrIdx < start.length) {
         end.push(start[arrIdx]);
         arrIdx += 1
     }
@@ -289,31 +268,26 @@ function addArray(start, end){
 }
 
 // function to see which players card has the bigger value
-function compareValues(pdeck, cdeck){
+function compareValues(pdeck, cdeck) {
     // if players top card is greater than comps top card
     // add both cards to end of player card array
-    if(playerdeck[0].value > computerdeck[0].value){
-        console.log(playerdeck);
-        console.log(computerdeck);
+    if (playerdeck[0].value > computerdeck[0].value) {
+        // console.log(playerdeck);
+        // console.log(computerdeck);
         let p1 = playerdeck.shift();
         let c1 = computerdeck.shift();
         playerdeck.push(p1);
         playerdeck.push(c1);
-    } 
+    }
     // if computers top card is greater than players top card
     // add both cards to end of computer card array
     else {
         let play1 = playerdeck.shift();
         let comp1 = computerdeck.shift();
         computerdeck.push(comp1);
-        computerdeck.push(play1); 
+        computerdeck.push(play1);
     }
     renderDeck(playerdeck, playerContainer);
-    renderDeck(computerdeck, computerContainer) 
+    renderDeck(computerdeck, computerContainer)
     updateCardCount();
 }
-
-
-
-
-// fix reset game to work
